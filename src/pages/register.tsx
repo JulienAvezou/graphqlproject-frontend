29c7e2ba5f3cf46cd0c8b8eacceaa564a1c50c94
Julien Avezou <julien.avezou@gmail.com>
2021-02-09 16:03:18 +0100

    register first part done

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,48 @@
-import React  from 'react'
+import React  from 'react';
+import { Formik, Form  } from 'formik'
+import { Box, Button } from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
 
 interface registerProps {}
 
 
 const Register: React.FC<registerProps> = ({}) => {
   return (
-    <div>register page</div>
+    <Wrapper variant="small">
+      <Formik 
+        initialValues={{username: "", password: ""}}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form> 
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"      
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"     
+              />
+            </Box>
+            <Button 
+              mt={4} 
+              type="submit" 
+              isLoading={isSubmitting} 
+              color="red"
+            >
+              register
+            </Button>
+          </Form>
+        )} 
+      </Formik>
+    </Wrapper>
   )
 };
 
