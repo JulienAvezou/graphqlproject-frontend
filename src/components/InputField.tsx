c7c65f0e78b190a03b777bd4d1ac74cd61c06983
Julien Avezou <julien.avezou@gmail.com>
2021-02-10 14:39:43 +0100

    setting up register form, cors config, graphql code generator for types & urql hooks, error handling

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,17 +3,21 @@ import { useField } from 'formik';
 import React, { InputHTMLAttributes } from 'react'
 
 type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  label: string,
-  placeholder: string,
+  label: string;
   name: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({label, size: _, ...props}) => {
+  // hook
   const [field, {error}] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input 
+        {...field}
+        {...props} 
+        id={field.name} 
+      />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null }
     </FormControl>
   );
